387d7695edc7d1e6e65cd16f9e7c918ccf713591
mdasif8393 <md.asif.8393@gmail.com>
2022-10-16 17:41:11 +0600

    add external modules

---
+++ index.js
@@ -0,0 +1,23 @@
+const {add} = require('./other');
+
+//core modules
+const http = require('http');
+
+//third party module --> underscore
+const _= require('underscore')
+
+const server = http.createServer(function(req, res){
+
+//   console.log("Server is running")
+
+});
+
+server.listen(5000); 
+
+// const result = add(12,12);
+// console.log(result);
+//ss
+var stooges = [{name: 'moe', age: 40}, {name: 'larry', age: 50}, {name: 'curly', age: 60}];
+
+const res = _.pluck(stooges, 'age');
+console.log(res);
---
+++ other.js
@@ -0,0 +1,5 @@
+var name = "programming hero";
+
+const add = (num1, num2) => num1 + num2;
+
+module.exports = { name, add };
